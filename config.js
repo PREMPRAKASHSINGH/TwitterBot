406ff9133f946c597573ec5c9c11b6dfd5164a98
Premprakash singh <premsingh.singh386@gmail.com>
2017-04-11 00:56:35 +0530

    changed configuration
    Changed configuration settings and added comments to paste your API key, secret and tokens. :)

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
-  consumer_key:         'Sn21aRqf7kM1exBgrXFlnBnd2',
-  consumer_secret:      'RmPuxRtvTYuB4fvW0L2zLCwfJjnnCNOeGuHsnMM54RiAKv5KcB',
-  access_token:         '1475077862-XEjiNRM0AG8m237K6tRTFhzA97PGIp53eysxTrx',
-  access_token_secret:  '8F1R64QyttePX35jutuEMgOwbwGVb2LBR2Si1lsDaM5By',
+  consumer_key:         '', //paste your consumer_key
+  consumer_secret:      '',  //paste your consumer_secret
+  access_token:         '',  //paste your access_token
+  access_token_secret:  '',  //paste your access_token_secret
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
